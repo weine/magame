564eab41524c760fba3333b2e06791beb21ed574
weine <momo.dark@gmail.com>
2015-05-04 17:52:00 +0800

    遊戲雛型02
    音效處理
    訊息框設計

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://192.168.1.107/magame/';
+$config['base_url'] = 'http://10.1.184.58/magame/';
 
 /*
 |--------------------------------------------------------------------------
